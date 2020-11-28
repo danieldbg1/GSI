27e32396b4e0d4bf6dcd7fc95e315947a05eda3a
xabware <xabware1@gmail.com>
2020-11-28 13:32:38 +0100

    añadido proyecto de reservas

---
+++ WebReservas/reservas/reservar/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Dueno,Direccion,Bar,Restaurante,Pub,Cliente
+admin.site.register(Dueno)
+admin.site.register(Direccion)
+admin.site.register(Bar)
+admin.site.register(Restaurante)
+admin.site.register(Pub)
+admin.site.register(Cliente)
+
+# Register your models here.
---
+++ WebReservas/reservas/reservar/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+# Create your models here.
+
+
+class Direccion(models.Model):
+    provincia=models.CharField(max_length=200)
+    ciudad=models.CharField(max_length=200)
+    calle=models.CharField(max_length=200)
+    numero=models.CharField(max_length=200)
+    codigo_postal=models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.calle) + " " + str(self.numero) + " " + str(self.codigo_postal) + " " + str(self.ciudad) + \
+               " (" + str(self.provincia) + ")"
+
+
+
+class Local(models.Model):
+    nombre = models.CharField(max_length=200)
+    descripcion = models.CharField(max_length=200)
+    direccion = models.OneToOneField(Direccion, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        abstract=False
+
+
+class Bar(Local):
+    especialidades=models.CharField(max_length=200)
+
+
+class Restaurante(Local):
+    precio_estimado= models.CharField(max_length=200)
+    cap_max_total= models.CharField(max_length=200)
+    cap_max_mesa= models.CharField(max_length=200)
+
+
+class Pub(Local):
+    hora_clausura=models.TimeField("hora de cierre")
+    hora_apertura=models.TimeField("hora de apertura")
+
+
+class Usuario(models.Model):
+    nick = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    nacimiento = models.DateTimeField("Fecha de nacimiento")
+
+    def __str__(self):
+        return self.nick
+
+    class Meta:
+        abstract = False
+
+
+
+class Dueno(Usuario):
+    locales = models.ManyToManyField(Local)
+
+
+class Cliente(Usuario):
+    class Meta:
+        abstract = False
