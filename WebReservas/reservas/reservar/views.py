42f4f147119d87ca27b5924b169d00970be06374
danieldbg1 <danieldbg1@gmail.com>
2021-01-28 10:48:34 +0100

    update

--- WebReservas/reservas/reservar/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.template import loader
-
-def index(request):
-    return render(request, 'reservar/index.html')
